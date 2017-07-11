9b913ad51c9565618b3ed048344b84b1de9f06fc
danielfmsouza <danielfmsouza@gmail.com>
2017-07-11 19:04:13 -0300

    continuing to add bluetooth in options menu

--- app/src/main/java/com/easyapps/teleprompter/presentation/GlobalSettingsActivity.java
+++ app/src/main/java/com/easyapps/teleprompter/presentation/GlobalSettingsActivity.java
@@ -1,11 +1,16 @@
 package com.easyapps.teleprompter.presentation;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.easyapps.teleprompter.R;
 import com.easyapps.teleprompter.presentation.components.GlobalSettingsFragment;
 import com.easyapps.teleprompter.presentation.components.TimerPreferenceFragment;
 import com.easyapps.teleprompter.presentation.helper.ActivityUtils;
+import com.easyapps.teleprompter.presentation.helper.PresentationConstants;
 
 /**
  * Created by daniel on 28/06/2017.
@@ -24,6 +29,25 @@ public class GlobalSettingsActivity extends Activity {
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new GlobalSettingsFragment())
                 .commit();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        switch (requestCode) {
+            case PresentationConstants.REQUEST_ENABLE_BT:
+                if (resultCode == RESULT_OK) {
+                    Toast.makeText(getBaseContext(), R.string.bluetooth_enabled_successfully,
+                            Toast.LENGTH_LONG).show();
+                }
+                else if (resultCode == RESULT_CANCELED){
+                    Toast.makeText(getBaseContext(), R.string.bluetooth_enabled_denied,
+                            Toast.LENGTH_LONG).show();
+                    GlobalSettingsFragment fragment = (GlobalSettingsFragment)
+                            getFragmentManager().findFragmentById(android.R.id.content);
+                    fragment.clickOnBluetoothCheckbox();
+                }
+                break;
+        }
     }
 
     @Override
--- app/src/main/java/com/easyapps/teleprompter/presentation/components/GlobalSettingsFragment.java
+++ app/src/main/java/com/easyapps/teleprompter/presentation/components/GlobalSettingsFragment.java
@@ -1,12 +1,21 @@
 package com.easyapps.teleprompter.presentation.components;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
+import android.view.View;
+import android.widget.Toast;
 
 import com.easyapps.teleprompter.R;
+import com.easyapps.teleprompter.infrastructure.communication.bluetooth.BluetoothScreenShareServer;
+import com.easyapps.teleprompter.presentation.helper.PresentationConstants;
 
+import static android.app.Activity.RESULT_OK;
 import static com.easyapps.teleprompter.R.xml.global_preferences;
 
 /**
@@ -16,25 +25,51 @@ import static com.easyapps.teleprompter.R.xml.global_preferences;
 
 public class GlobalSettingsFragment extends PreferenceFragment {
 
+    private BluetoothScreenShareServer screenShareServer;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(global_preferences);
+        screenShareServer = new BluetoothScreenShareServer(BluetoothAdapter.getDefaultAdapter());
 
-//        PreferenceScreen preferenceScreen = this.getPreferenceScreen();
-//        PreferenceCategory bluetoothCategory =
-//                (PreferenceCategory)preferenceScreen.getPreference(2);
-//
-//        // create preferences manually
-//        PreferenceCategory preferenceCategory = new PreferenceCategory(preferenceScreen.getContext());
-//        preferenceCategory.setTitle(getResources().getString(R.string.pref_bluetoothPairedDevices));
-//        bluetoothCategory.addPreference(preferenceCategory);
+        PreferenceScreen preferenceScreen = this.getPreferenceScreen();
 
-//        Preference preference = new Preference(preferenceScreen.getContext());
-//        preferencey.setTitle("yourTitle");
-//        //do anything you want with the preferencey here
-//        preferenceCategory.addPreference(preference);
+        PreferenceCategory pcBluetooth = new PreferenceCategory(preferenceScreen.getContext());
+        pcBluetooth.setTitle(getResources().getString(R.string.pref_bluetooth));
+        preferenceScreen.addPreference(pcBluetooth);
+
+        final CheckBoxPreference cbpBluetoothEnabled =
+                new CheckBoxPreference(preferenceScreen.getContext());
+        cbpBluetoothEnabled.setChecked(screenShareServer.isProtocolEnabled());
+        cbpBluetoothEnabled.setKey(getResources().getString(R.string.pref_isBluetoothEnabled));
+        cbpBluetoothEnabled.setTitle(R.string.pref_isBluetoothEnabled);
+        cbpBluetoothEnabled.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                if (cbpBluetoothEnabled.isChecked()){
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, PresentationConstants.REQUEST_ENABLE_BT);
+                }
+                else{
+                    screenShareServer.disable();
+                }
+                return true;
+            }
+        });
+        pcBluetooth.addPreference(cbpBluetoothEnabled);
+    }
+
+    public void clickOnBluetoothCheckbox(){
+        PreferenceScreen preferenceScreen = this.getPreferenceScreen();
+        CheckBoxPreference cbpBluetooth = (CheckBoxPreference)
+                preferenceScreen.findPreference(
+                        getResources().getString(R.string.pref_isBluetoothEnabled));
+
+        if (cbpBluetooth != null){
+            cbpBluetooth.setChecked(!cbpBluetooth.isChecked());
+        }
     }
 }
